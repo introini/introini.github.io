d85ae488a447d1e0326055df72f3dac5a59c82ac
mike <mintroini@cs.columbia.edu>
2026-09-06 11:09:50 -0700

    moved old files

--- coms3134/Lab1.java
+++
@@ -1,8 +0,0 @@
-public class Lab1{
-	public static void main(String[] args) {
-		String file = args[0];
-		for(int i = 0; i<file.length(); i++) {
-			System.out.println(file.charAt(i));
-		}
-	}
-}
--- coms3134/mbi2105homework3/Problem1.java
+++
@@ -1,7 +0,0 @@
-public class Problem1 {
-  public static void main (String[] args) {
-    String postFix = "34 2 - 5 *";
-    ExpressionTree tree = new ExpressionTree(postFix);
-
-  }
-}
